9816c09f144aabecabce0cfb309f0bf7f166ffb2
Kyle Bridburg <kbridbur@mit.edu>
2018-05-02 16:18:44 -0400

    Add scene with rocket

---
+++ Assets/Thruster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thruster : MonoBehaviour {
+
+	void Start(){
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.AddForce (new Vector2(0, Thrust));
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		Debug.Log ("wtf");
+	}
+
+	Rigidbody2D rb;
+	public float Thrust=0;
+}
