196255162a0a55317b2e50464a0b306171336087
Eric LaCava <elacava@mit.edu>
2018-05-09 23:30:25 -0400

    Added help pop-ups for blocks

---
+++ Assets/Eric/Scripts/Help.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Help : MonoBehaviour {
+
+    public Text helpTextObject;
+    public string helpText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseEnter ()
+    {
+        helpTextObject.text = helpText;
+        Debug.Log("Touched");
+    }
+
+    void OnMouseExit ()
+    {
+        helpTextObject.text = "";
+    }
+}
