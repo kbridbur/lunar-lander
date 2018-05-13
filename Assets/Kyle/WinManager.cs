28e03e2c4305030452d9319fa34a4be601909546
Kyle Bridburg <kbridbur@mit.edu>
2018-05-13 18:46:27 -0400

    Add some boiler plate for win/loss/levels

---
+++ Assets/Kyle/WinManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinManager : MonoBehaviour {
+
+	void Start(){
+		WinText.SetActive (false);
+		LoseText.SetActive (false);
+	}
+
+	void WinGame(){
+		WinText.SetActive (true);
+		Time.timeScale = 0;
+	}
+
+	void LoseGame(){
+		LoseText.SetActive (true);
+		Time.timeScale = 0;
+	}
+
+	void SetLevel(int l){
+		level = l;
+	}
+
+	public GameObject Lander;
+	public GameObject GameManager;
+	public int level = 1;
+	public GameObject WinText;
+	public GameObject LoseText;
+}
